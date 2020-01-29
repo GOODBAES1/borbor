5d97caa8a6743f085ce37ce48f9cb80884e62a61
GOODBAES1 <59509018+GOODBAES1@users.noreply.github.com>
2020-01-29 06:50:14 +0600

    Initial commit

---
+++ lib/viewmodels/startup_view_model.dart
@@ -0,0 +1,23 @@
+import 'package:compound/constants/route_names.dart';
+import 'package:compound/services/authentication_service.dart';
+import 'package:compound/services/navigation_service.dart';
+import 'package:compound/viewmodels/base_model.dart';
+
+import '../locator.dart';
+
+class StartUpViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+  locator<AuthenticationService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+
+
+  Future handleStartUpLogic() async {
+    var hasLoggedInUser = await _authenticationService.isUserLoggedIn();
+
+    if (hasLoggedInUser) {
+      _navigationService.navigateTo(HomeViewRoute);
+    } else {
+      _navigationService.navigateTo(LoginViewRoute);
+    }
+  }
+}
